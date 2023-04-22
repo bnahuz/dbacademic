2a7b65bf705ac4b9f598bd1dc665ea06b3566b9e
Breno Nahuz <breno.nahuz@hyperlocal.com.br>
2023-04-22 18:39:15 -0300

    add: Adicionado UFP

--- plugins/institutes/UFPI.py
+++ plugins/institutes/UFPI.py
@@ -7,9 +7,6 @@ from plugins.utils.mongo import get_mongo_db, insert_many, drop_collection
 #Dados desatualizados desde 2018
 ufpi_consumer = CkanConsumer('dados.ufpi.br', secure=True) 
 
-#https://dados.ufpi.br/dataset/efe01f87-fd4c-4cc1-a081-5c8d29cf7be0/resource/00658685-cd1b-47f4-9f42-107c45a1e4e1/download/discentesgraduacaoativos2bimestre.csv
-
-
 #Curso
 def etl_courses():
     cursos_ufpi = ufpi_consumer.request('afb0de00-8f39-4f9c-9197-4364f7312a98')
