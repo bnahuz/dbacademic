399804cf7e79404aaabfe9735a17e05b71527850
Sergio Costa <sergio.costa@ufma.br>
2023-04-25 08:53:36 -0300

    estado que o breno dia 24

--- plugins/utils/dag_utils.py
+++ plugins/utils/dag_utils.py
@@ -7,6 +7,11 @@ from plugins.institutes.ufca import ufca
 from plugins.utils.mongo import drop_collection
 from airflow.models.baseoperator import chain
 
+from plugins.consumers.CkanConsumer import CkanConsumer
+
+
+from plugins.utils.mongo import get_mongo_db, insert_many, drop_collection
+
 def dynamic_drop(task_id:str, insitute:str, collection:str, dag:DAG):
     return PythonOperator(
         task_id=task_id,
@@ -16,25 +21,48 @@ def dynamic_drop(task_id:str, insitute:str, collection:str, dag:DAG):
     )
 
 
-def dynamic_create_dag(dag_id:str, institute, collections:list, schedule_interval, start_date, default_args):
+def save_collection_closure(instituicao,collection, params):
+    def cl ():
+        return save_collection(instituicao,collection, consummer, mapper)
+    return cl
+
+def save_collection(instituicao, colecao, params):
+    dados = consumer.request()
+    #dados = mapper_all(mapper, data)
+    if (params["consumer"] == "ckan"):
+            ckan_params = params["consumer_params"]
+            consumer = CkanConsumer(ckan_params["main_url"])
+            dados = consumer.request(ckan_params["resource_id"]) # nao sei se precisaria do pandas
+            dados = dados.to_dict('records')
+            dados = dados[1:10]
+            print (dados)           
+            #insert_many(get_mongo_db(instituicao),colecao,dados)
+            return f"Inserted {colecao} in {instituicao} {dados}"
+    else:
+            return "fail to inserted"
+
+
+
+def dynamic_create_dag(dag_id:str, institute, collections, schedule_interval, start_date, default_args):
     dag = DAG(
         f'{dag_id}_etl',
         default_args=default_args,
-        description=f'A simple DAG to extract data from {institute.name} API',
+        description=f'A simple DAG to extract data from {institute} API',
         schedule_interval=schedule_interval,
         start_date=start_date
     )
 
     drop_task = []
-    for collection in collections:
-        task = dynamic_drop(f'drop_{collection}', str(institute.name), collection, dag)
+    for collection, paramns in collections.items():
+        task = dynamic_drop(f'drop_{collection}', str(institute), collection, dag)
         drop_task.append(task)
 
+    
     elt_task = []
-    for collection in collections:
+    for collection, params in collections.items():
         task = PythonOperator(
             task_id=f'run_intake_{collection}',
-            python_callable=getattr(institute, f'etl_{collection}'),
+            python_callable=save_collection_closure (institute,collection, params),
             dag=dag,
         )
         elt_task.append(task)
